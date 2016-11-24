2909b282c806d0c78225d547d680f3b5b78e3e59
Kyle Garrison <kgarrison343@gmail.com>
2016-11-23 20:02:02 -0500

    Implement generating l-strings for several generations.

--- generator.lisp
+++ generator.lisp
@@ -5,7 +5,12 @@
       (if (string= c (caar rules))
 	  (second (first rules))
 	  (apply-rules c (rest rules)))
-      c))
+      (string c)))
 
 (defun generate-l-string (axiom rules)
   (list-to-string (map 'list #'(lambda (c) (apply-rules c rules)) axiom)))
+
+(defun iterate-l-string (axiom rules iterations)
+  (if (= 0 iterations)
+      axiom
+      (iterate-l-string (generate-l-string axiom rules) rules (- iterations 1))))
